da997a7afe007b359dbca1375dbac7c2745b816e
alfateam123 <alfateam123@gmail.com>
2015-01-17 01:54:45 +0100

    Improve configuration file reading. tests included.
    Documentation about Miyuki conf file changes will be added later.

--- celty/conf.py
+++ celty/conf.py
@@ -10,3 +10,29 @@ class ConfReader(object):
 
     def parseConf(self):
         self.miyuki_data = yaml.load(open(self.miyuki_conf_path))
+
+    def extractShowDownloadInfo(self, show_name):
+        try:
+            return [series_data for series_data in self.miyuki_data["series"] 
+                       if series_data["name"] == show_name][-1]["download"]
+        except IndexError:
+            #as no element has been found, the show has not been found
+            raise ValueError("Show {0} does not exists!".format(show_name))
+        except KeyError:
+        	#no "download" section has been declared for this show
+        	#use global definitions
+        	return self.miyuki_data["download"]
+
+    @property
+    def globalDownloadDir(self):
+        return self.miyuki_data["download"]["downloadDir"]
+
+    @property
+    def globalSeedTime(self):
+        return self.miyuki_data["download"]["seedTime"]
+    
+    def downloadDir(self, show_name):
+        return self.extractShowDownloadInfo(show_name)["downloadDir"]
+
+    def seedTime(self, show_name):
+        return self.extractShowDownloadInfo(show_name)["seedTime"]
--- tests/test_conf.py
+++ tests/test_conf.py
@@ -11,6 +11,28 @@ class TestConfigurationReader(unittest.TestCase):
     def setUp(self):
         self.reader = conf.ConfReader(CONFIGURATION)
 
+    def test_dataAreImportedCorrectly(self):
+        try:
+            self.assertRaises(AttributeError, self.reader.miyuki_data)
+            self.assertTrue(False)
+        except:
+            self.assertNotEqual(self.reader.miyuki_data, None)
+            self.assertEqual(self.reader.miyuki_data["notifications"], {"enabled": True})
+
+    def test_globalDownloadDirIsImportedCorrectly(self):
+        self.assertEqual(self.reader.globalDownloadDir, "F:/global_folder")
+
+    def test_globalSeedingTimeIsImportedCorrectly(self):
+        self.assertEqual(self.reader.globalSeedTime, 10)
+
+    def test_downloadDirPerSerieIsImportedCorrectly(self):
+        self.assertEqual(self.reader.downloadDir("Nisekoi"), "F:/serie_tv/False_Love")
+        self.assertEqual(self.reader.downloadDir("Sora no Woto"), "F:/global_folder")
+
+    def test_seedTimePerSerieIsImportedCorrectly(self):
+        self.assertEqual(self.reader.seedTime("Shigatsu"), 25)
+        self.assertEqual(self.reader.seedTime("Sora no Woto"), 10)
+
 class TestConfigurationReaderErrors(unittest.TestCase):
     def setUp(self):
         self.reader = conf.ConfReader(CONFIGURATION)
@@ -18,10 +40,11 @@ class TestConfigurationReaderErrors(unittest.TestCase):
     def test_invalidMiyukiPath(self):
         self.assertRaises(IOError, conf.ConfReader, ('lolinvalid.conf'))
 
-    def test_dataAreImportedCorrectly(self):
-        try:
-            self.assertRaises(AttributeError, self.reader.miyuki_data)
-            self.assertTrue(False)
-        except:
-        	self.assertNotEqual(self.reader.miyuki_data, None)
-        	self.assertEqual(self.reader.miyuki_data["notifications"], {"enabled": True})
+    def test_downloadDirPerSerieNotFound(self):
+        self.assertRaises(ValueError, self.reader.downloadDir, "RoxasPls")      
+        self.assertRaises(TypeError, self.reader.downloadDir)
+
+    def test_seedTimePerSerieNotFound(self):
+        self.assertRaises(ValueError, self.reader.seedTime, "RoxasPls")
+        self.assertRaises(TypeError, self.reader.seedTime)
+
