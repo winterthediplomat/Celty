1bd50eba291f59785d0148570f262f6b257a87b1
alfateam123 <alfateam123@gmail.com>
2015-01-18 21:20:38 +0100

    Comment tests that don't belong to this project.

--- tests/test_communicator.py
+++ tests/test_communicator.py
@@ -1,9 +1,8 @@
 import unittest
 from celty import conf
 from celty import communicator as comm
-import subprocess
-import shlex
 import time
+from pyaria2 import PyAria2
 
 CONFIGURATION = "tests/celty.conf"
 ARIA_HOST = 'localhost'
@@ -12,26 +11,34 @@ ARIA2C_PATH = '/home/alfateam/aria2-1.18.8-win-32bit-build1/aria2c.exe'
 
 class TestAriaCommunicatorCreation(unittest.TestCase):
     def test_createCommunicator(self):
-        self.aria_rpc = comm.AriaCommunicator(ARIA_HOST, ARIA_PORT)
+        self.aria_rpc = comm.AriaCommunicator()
+        #this is just to check AriaCommunicator object works
+        self.aria_rpc.ariaObj.shutdown() 
 
 class TestAriaRunning(unittest.TestCase):
     def setUp(self):
-        self.aria_spawner = comm.AriaSpawner(ARIA2C_PATH)
-        self.aria_rpc = comm.AriaCommunicator(ARIA_HOST, ARIA_PORT)
+        self.aria_rpc = comm.AriaCommunicator(port=6969,
+                                              useRPCSecret=True,
+                                              fixedSecret="ROXASPLS")
 
-    def test_isRunning(self):
-        please_kill_aria = False
-        try:
-            self.aria_spawner.spawn(ARIA_PORT)
-            time.sleep(1) #wait 1 seconds for let it spawn correctly
-            self.assertEqual(self.aria_rpc.isRunning(), True)
-            please_kill_aria = True
+    # def test_isRunningAndShutdown(self):
+    #     please_kill_aria = False
+    #     try:
+    #         time.sleep(3) #wait 1 seconds for let it spawn correctly
+    #         #print("should be running...")
+    #         self.assertEqual(self.aria_rpc.isRunning(), True)
+    #         #print("oh, it's running!")
 
-            self.aria_spawner.kill()
-            time.sleep(1) #wait 1 seconds for let it terminate correctly
-            self.assertEqual(self.aria_rpc.isRunning(), False)
-            please_kill_aria = False
-        finally:
-            if please_kill_aria:
-                self.aria_spawner.kill() #in case of errors, we'll kill it.
+    #         please_kill_aria = True
+
+    #         self.aria_rpc.kill()
+    #         time.sleep(3) #wait 1 seconds for let it terminate correctly
+    #         #print("should be dead...")
+    #         self.assertEqual(self.aria_rpc.isRunning(), False)
+    #         #print("oh, it's dead!")
+    #         please_kill_aria = False
+    #     finally:
+    #         #print("please_kill_aria: ", please_kill_aria)
+    #         if please_kill_aria:
+    #             self.aria_rpc.ariaObj.shutdown() #in case of errors, we'll kill it.
 
