fd8a44100a5502955f9b43b3d1a6b6d3c5b6955d
alfateam123 <alfateam123@hotmail.it>
2015-09-18 02:02:34 +0200

    skip a test if aria2c cannot be found

--- tests/test_communicator.py
+++ tests/test_communicator.py
@@ -5,11 +5,14 @@ import time
 from pyaria2 import PyAria2
 from string import ascii_letters as letters
 
+import os
+
 CONFIGURATION = "tests/celty.conf"
 ARIA_HOST = 'localhost'
 ARIA_PORT = 6800
 ARIA2C_PATH = '/home/alfateam/aria2-1.18.8-win-32bit-build1/aria2c.exe'
 
+@unittest.skipIf(not os.path.exists(ARIA2C_PATH), "aria2 is not installed or not available on this system. skipping...")
 class TestAriaCommunicatorCreation(unittest.TestCase):
     def test_createCommunicator(self):
         self.aria_rpc = comm.AriaCommunicator(port=9999) #travis uses an outdated (pre-1.8) aria2.
